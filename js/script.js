d06764f7f9448520eeb2bb5572e8ed019595f292
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-08 17:28:31 +0100

    adapt old js to new

--- js/script.js
+++ js/script.js
@@ -17,32 +17,43 @@ Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagi
 "use strict";
 console.clear();
 
+//dati e var
 const priceKm = 0.21;
-console.log ("Benvenuti sul nostri treno, la informiamo che la tariffa per viaggiare sulle nostre tratte è di: " + priceKm + " al km.");
-
 const juniorDiscount = 20;
 const juniorAge = 18;
-console.log ("Lo sconto del " + juniorDiscount + "% è applicato a tutti i biglietti acquistati da persone con un età inferiore ai " + juniorAge + " anni di età.");
-
 const seniorDiscount = 40;
 const seniorAge = 65;
-console.log ("Lo sconto del " + seniorDiscount + "% è applicato a tutti i biglietti acquistati da persone con un età uguale o superiore ai " + seniorAge + " anni di età.");
+const kminput = document.getElementById("km");
+const ageinput = document.getElementById("age");
+const myButton = document.querySelector(".btn");
 
-const km = parseInt(prompt("Per acquistare un biglietto inserisca di seguito la lunghezza della tratta che deve percorre in km."));
-console.log ("La tratta che deve percorre è di " + km + "km");
+let km = 0;
+let age = 0;
 
-const age = parseInt(prompt("Inserisca di seguito la sua età cosi da poter verificare se ha diritto a uno degli sconti della nostra linea ferroviaria"));
+function dati() {
+km = kminput.value;
+age = ageinput.value;
+}
 
+
+
+myButton.addEventListener("click", dati);
+
+//calcolo prezzo al km
 let price = km * priceKm;
-console.log ("Il prezzo del biglietto per un utente che non ha diritto ad alcuno sconto è di: " + Math.round(price * 100) / 100 + "€");
+
+//prendo l'elemento dove va inserito il messaggio segurente
+const details = document.querySelector("details");
+console.log (details);
+details.innerHTML = "Il prezzo del suo bigliettoè: " + Math.round(price * 100) / 100 + "€<br>" + details;
 
 let discount = 0.00;
 
-if (age < juniorAge){
+if (age.value < juniorAge){
     discount = price * 20 / 100;
     price = price - discount;
     console.log ("Per via della sua età sul suo biglietto verrà applicato uno sconto di " + Math.round(discount * 100) / 100 + "€, pertanto il prezzo finale del suo biglietto è di: " + Math.round(price * 100) / 100 + "€");
-} else if(age >= seniorAge){
+} else if(age.value >= seniorAge){
     discount = price * 40 / 100;
     price = price - discount;
     console.log ("Per via della sua età sul suo biglietto verrà applicato uno sconto del " + Math.round(discount * 100) / 100 + "€, pertanto il prezzo finale del suo biglietto è di: " + Math.round(price * 100) / 100 + "€");
